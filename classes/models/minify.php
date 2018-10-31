7256640db043800d481a92472c90aa0d86256d34
technote <technote.space@gmail.com>
2018-11-01 05:25:33 +0900

    v1.1.33

--- classes/models/minify.php
+++ classes/models/minify.php
@@ -2,7 +2,7 @@
 /**
  * Technote Models Minify
  *
- * @version 1.1.13
+ * @version 1.1.33
  * @author technote-space
  * @since 1.0.0
  * @copyright technote All Rights Reserved
--- classes/traits/presenter.php
+++ classes/traits/presenter.php
@@ -2,7 +2,7 @@
 /**
  * Technote Traits Presenter
  *
- * @version 1.1.25
+ * @version 1.1.33
  * @author technote-space
  * @since 1.0.0
  * @copyright technote All Rights Reserved
