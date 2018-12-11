7ce458a286f03371b84e5f2a2f1f9c24c7eeebb5
technote-space <technote.space@gmail.com>
2018-12-12 04:55:38 +0900

    開発バージョンチェックの修正

--- src/technote.php
+++ src/technote.php
@@ -341,16 +341,11 @@ class Technote {
 		$update_info_file_url = $this->get_config( 'config', 'update_info_file_url' );
 		if ( ! empty( $update_info_file_url ) ) {
 			if ( $this->filter->apply_filters( 'check_update' ) ) {
-				$key   = $this->plugin_name . '-setup_update';
-				$check = get_transient( $key );
-				if ( ! $check ) {
 					\Puc_v4_Factory::buildUpdateChecker(
 						$update_info_file_url,
 						$this->plugin_file,
 						$this->plugin_name
 					);
-					set_transient( $key, true, 15 * 60 );
-				}
 			}
 		} else {
 			$this->setting->remove_setting( 'check_update' );
