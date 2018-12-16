346a0df8f45b4cb68525e189b88516772f89de95
technote <technote.space@gmail.com>
2018-12-16 18:27:47 +0900

    v2.4.3

--- src/classes/models/lib/upgrade.php
+++ src/classes/models/lib/upgrade.php
@@ -2,10 +2,11 @@
 /**
  * Technote Classes Models Lib Upgrade
  *
- * @version 2.4.1
+ * @version 2.4.3
  * @author technote-space
  * @since 2.4.0
  * @since 2.4.1 Added: show_plugin_update_notices method
+ * @since 2.4.3 Fixed: get plugin upgrade notice from plugin directory
  * @copyright technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
  * @link https://technote.space
@@ -118,6 +119,7 @@ class Upgrade implements \Technote\Interfaces\Loader {
 	/**
 	 * show plugin upgrade notices
 	 * @since 2.4.1
+	 * @since 2.4.3 Fixed: get plugin upgrade notice from plugin directory
 	 */
 	public function show_plugin_update_notices() {
 		add_action( 'in_plugin_update_message-' . $this->app->define->plugin_base_name, function ( $data, $r ) {
